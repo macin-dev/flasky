33a95acf91b92bcd3f71e39aba08b00b9fd54c76
macin-dev <ernesto.dev24@gmail.com>
2026-07-31 20:39:00 -0600

    Chapter 4: Web forms with Flask-WTF (4a)

--- hello.py
+++ hello.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create an instance of a Flask application
 # It defines the core center for Web requests
 # coming from the user's browser
 app = Flask(__name__)
+# WTF mechanism for security reasons
+app.config['SECRET_KEY'] =  'hard to guess string'
 
 # Initialize Bootstrap extension by passing in
 # the instance of the app
@@ -14,6 +19,11 @@ bootstrap = Bootstrap(app)
 
 # Initialize extension to format dates and times
 moment = Moment(app)
+
+# NameForm inherits from FlaskForm
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # Error handling, it returns a tuple with the
 # status code for the response to the client
@@ -30,9 +40,14 @@ def internal_server_error(e):
 # a function view, a handler to be called
 # when an event is triggered, in this case a user
 # visiting the root of the web application
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.now(timezone.utc))
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.now(timezone.utc), form=form, name=name)
 
 # It handles dynamic routes
 @app.route('/user/<name>')
