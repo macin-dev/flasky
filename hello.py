d4e823c40f401b7214818a055df1b2a033fb93dc
macin-dev <ernesto.dev24@gmail.com>
2026-08-01 07:55:56 -0600

    Chapter 4: Message flashing (4c)

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
@@ -44,6 +44,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('You have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', current_time=datetime.now(timezone.utc), form=form, name=session.get('name'))
