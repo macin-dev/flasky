dbeae18164be1ea8d86543bc24d3a238a5804d60
macin-dev <ernesto.dev24@gmail.com>
2026-08-07 11:16:39 -0600

    Chapter 6: Asynchronous emails (6b)

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired 
 from flask_migrate import Migrate   
 from flask_mail import Mail, Message
+from threading import Thread
 
 # Create an instance of a Flask application
 # It defines the core center for Web requests
@@ -69,12 +70,18 @@ class User(db.Model):
         return '<User %r>' % self.username
     
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 # Initialize Bootstrap extension by passing in
 # the instance of the app
