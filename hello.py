097e229cf33779da2b84b80b32feef7f5a8487e1
macin-dev <ernesto.dev24@gmail.com>
2026-08-07 11:04:06 -0600

    Chapter 6: Email support with Flask-Mail (6a)

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired 
 from flask_migrate import Migrate   
+from flask_mail import Mail, Message
 
 # Create an instance of a Flask application
 # It defines the core center for Web requests
@@ -19,10 +20,19 @@ app.config['SECRET_KEY'] =  'hard to guess string'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = f"Flasky Admin <{os.environ.get('MAIL_USERNAME')}>"
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 # It represents the Role model inheriting from db.Model
 class Role(db.Model):
@@ -59,6 +69,12 @@ class User(db.Model):
         return '<User %r>' % self.username
     
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 # Initialize Bootstrap extension by passing in
 # the instance of the app
@@ -102,6 +118,8 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User','mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
